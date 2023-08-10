d9b8765be143d80a5170536f76cb071471fa8dc9
Avangardio <12lol34lol56lol@gmail.com>
2023-08-10 22:12:33 +0300

    EX-1-2

--- partners/index.php
+++ partners/index.php
@@ -4,6 +4,6 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
 $APPLICATION->SetTitle("Условия сотрудничества");
 ?>
-
+<b>Ну то есть типа? Ну типа...</b>
 <p><a href="<?=SITE_DIR?>">Вернуться на главную страницу</a></p>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
