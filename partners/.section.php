09900b27a67bda7bfbaf2ca83f846896b2ce25f4
Avangardio <12lol34lol56lol@gmail.com>
2023-08-10 22:09:20 +0300

    EX-1-2

--- partners/.section.php
+++ partners/.section.php
@@ -3,5 +3,6 @@ $sSectionName = "Как стать партнером";
 $arDirProperties = array(
     "description" => "Услуги компании XXXX",
     "keywords" => "услуги, компания XXXX",
+    "TITLE" => "Как стать парнтром1"
 );
 ?>
