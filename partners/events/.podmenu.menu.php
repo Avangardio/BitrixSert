13592c6d3b1690c496bb5e5e152a3e7286053c63
Avangardio <12lol34lol56lol@gmail.com>
2023-08-11 12:55:01 +0300

    EX-1-6

---
+++ partners/events/.podmenu.menu.php
@@ -0,0 +1,19 @@
+<?php
+
+$aMenuLinks = array(
+    array(
+        "Новая страница 1",
+        "/test_menu/novaya_stranitsa_1.php",
+        array(),
+        array(),
+        ""
+    ),
+    array(
+        "Новая страница 2",
+        "/test_menu/novaya_stranitsa_2.php",
+        array(),
+        array(),
+        ""
+    )
+);
+
--- partners/index.php
+++ partners/index.php
@@ -1,7 +1,6 @@
 <?
 define("NEED_AUTH", true);
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-
 $APPLICATION->SetTitle("Условия сотрудничества", ["SET_TITLE" => "N"]);
 ?>
 <b>Ну то есть типа? Ну типа...</b>
